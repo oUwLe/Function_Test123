5661b442be4d928527b2d53fc6328ab9ec574a50
oUwLe <sebastian.ljung18@hotmail.com>
2025-05-28 16:08:32 +0200

    alot

--- js/index.js
+++ js/index.js
@@ -1,33 +1,43 @@
 
-function add(numberOne, numberTwo) {
+
+function addNumbers(inputNumberOne, inputNumberTwo) {
 
     console.log("Add");
-    console.log("numberOne är: ", numberOne);
-    console.log("numberTwo är: ", numberTwo);
+    console.log("numberOne is: ", inputNumberOne);
+    console.log("numberTwo is: ", inputNumberTwo);
 
-        let sum = numberOne + numberTwo;
+        let sum = Number(inputNumberOne) + Number(inputNumberTwo);  //Kan också lösa genom att köra Number(inputNumberOne) så det inte är en sträng utan ne siffra
 
             console.log("numberOne + numberTwo = " + sum);
 }
 
-function subtract(numberOne, numberTwo) {
+function subtractNumbers(inputNumberOne, inputNumberTwo) {
 
     console.log("Subtract");
-    console.log("numberOne är: ", numberOne);
-    console.log("numberTwo är: ", numberTwo);
-        let sum = numberOne - numberTwo;
+    console.log("numberOne is: ", inputNumberOne);
+    console.log("numberTwo is: ", inputNumberTwo);
+
+        let sum = inputNumberTwo - inputNumberTwo;
 
             console.log("numberOne - numberTwo = " + sum);
 }
 
-let operator = "subtract"; {
-} 
-if (operator == "subtract") {
-    subtract(80, 40);
-} 
-else if (operator == "add") {
-    add(20, 20);
+let inputNumberOne = prompt("Choose first number");
+    console.log("First number: ", inputNumberOne);
+const inputOperator = prompt("Addition (+) or subtraction (-)");
+    console.log("Operator:", inputOperator);
+let inputNumberTwo = prompt("Choose second number");    //parseInt hjälper datorn förstå att det är siffror inte strängar
+    console.log("Second number: ", inputNumberTwo);
+
+if (inputOperator == "+") {
+    addNumbers(inputNumberOne, inputNumberTwo);
+} else if(inputOperator == "-") {
+    subtractNumbers(inputNumberOne, inputNumberTwo);
 }
 
-// add(5, 10);
-// subtract(55, 25);
+// Add Two Numbers Entered by the User
+// Use prompt() to get input from the user.
+
+// Convert the input from strings to integers using parseInt()
+
+//This snippet uses the prompt() function to capture user inputs. Since prompt() returns strings, parseInt() is utilized to convert these strings to integers. The numbers are then added, and the result is displayed to the user via alert().
